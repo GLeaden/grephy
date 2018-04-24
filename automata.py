d1bbf7da5d68f40eb657940d7458d493bebba8d5
G <goleaden@gmail.com>
2018-04-24 14:19:12 -0400

    normalized function names so my python linter would stop warning me

--- automata.py
+++ automata.py
@@ -1,52 +1,52 @@
 class DFA():
-  def __init__(self, language=set()):
-    self.validStates = set() # Set of valid states.
-    self.language = language # Set of valid characters.
-    self.transitions = {} # Dictionary of transition functions.
-    self.startState = None # Starting state.
-    self.acceptStates = set() # Set of accepting states. Subset of validStates. DFAs will have one.
+    def __init__(self, language=set()):
+        self.valid_states = set() # Set of valid states.
+        self.language = language # Set of valid characters.
+        self.transitions = {} # Dictionary of transition functions.
+        self.start_state = None # Starting state.
+        self.accept_states = set() # Set of accepting states. Subset of valid_states.
 
-  def setStartState(self, state):
-    """Sets the given state as the startState, adds to validStates"""
-    self.startState = state
-    self.validStates.add(state)
+    def set_start_state(self, state):
+        """Sets the given state as the start_state, adds to valid_states"""
+        self.start_state = state
+        self.valid_states.add(state)
 
-  def addAcceptState(self, state):
-    """Adds the given state to the set of AcceptStates"""
-    if state in self.validStates:
-      self.acceptStates.add(state)
-    else:
-      print("accept state is not contained in validStates!")
+    def add_accept_state(self, state):
+        """Adds the given state to the set of Accept_states"""
+        if state in self.valid_states:
+          self.accept_states.add(state)
+        else:
+          print "accept state is not contained in valid_states!"
 
-  def addTransition(self, beginState, endState, symbol):
+    def add_transition(self, begin_state, end_state, symbol):
+            """Adds a new transition function to the transitions dictionary.
+
+            Keyword Arguments:
+            begin_state -- the state in which you begin transition
+            end_state -- the state in which you are in when transition ends
+            symbol -- the symbol which triggers this transition
+            """
+            self.valid_states.add(begin_state)
+            self.valid_states.add(end_state)
+            if begin_state in self.transitions:
+              if end_state in self.transitions[begin_state]:
+                self.transitions[begin_state][end_state] = symbol
+            else:
+              self.transitions[begin_state] = {end_state: symbol}
+
+class NFA(DFA):
     """Adds a new transition function to the transitions dictionary.
 
     Keyword Arguments:
-    beginState -- the state in which you begin transition
-    endState -- the state in which you are in when transition ends
-    symbol -- the symbol which triggers this transition
-    """
-    self.validStates.add(beginState)
-    self.validStates.add(endState)
-    if beginState in transitions:
-      if endState in transitions[beginState]:
-        transitions[beginState][endState] = symbol
-    else:
-      transitions[beginState] = {endState: symbol}
-
-class NFA(DFA):
-  """Adds a new transition function to the transitions dictionary.
-
-    Keyword Arguments:
-    beginState -- the state in which you begin transition
-    endState -- the state in which you are in when transition ends
+    begin_state -- the state in which you begin transition
+    end_state -- the state in which you are in when transition ends
     symbols -- the symbols which trigger this transition
     """
-  def addTransition(self, beginState, endState, symbols):
-    self.validStates.add(beginState)
-    self.validStates.add(endState)
-    if beginState in transitions:
-      if endState in transitions[beginState]:
-        transitions[beginState][endState] = symbols
-    else:
-      transitions[beginState] = {endState: symbols}
+    def add_transition(self, begin_state, end_state, symbols):
+        self.valid_states.add(begin_state)
+        self.valid_states.add(end_state)
+        if begin_state in self.transitions:
+            if end_state in self.transitions[begin_state]:
+                self.transitions[begin_state][end_state] = symbols
+        else:
+            self.transitions[begin_state] = {end_state: symbols}
--- grephy.py
+++ grephy.py
@@ -5,39 +5,37 @@ create main function with argument parsing capabilities
 """
 
 def main():
-  args = parseArgs()
-  alphabet = learnAlphabet(args.inputFile)
-  for i in alphabet:
-    print i,
-    print ord(i)
+    args = parse_args()
+    alphabet = learn_alphabet(args.input_file)
+    for i in alphabet:
+        print i,
+        print ord(i)
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='search regex pattern in file.')
+    parser.add_argument('RegEx', metavar='REGEX-PATTERN', type=str,
+                        help="the regular expression pattern to search"+
+                        "input file with")
+    parser.add_argument('input_file', metavar='INPUT-FILE', type=argparse.FileType('r'),
+                        help="the file to test regex against")
+    parser.add_argument("-n", type=argparse.FileType('rw'),
+                        help="enable NFA output to specified file")
+    parser.add_argument("-d", type=argparse.FileType('rw'),
+                        help="enable DFA output to specified file")
+    args = parser.parse_args()
+    return args
 
-
-def parseArgs():
-  parser = argparse.ArgumentParser(description='search regex pattern in file.')
-  parser.add_argument('RegEx', metavar='REGEX-PATTERN', type=str,
-                    help="the regular expression pattern to search"+
-                    "input file with")
-  parser.add_argument('inputFile', metavar='INPUT-FILE', type=argparse.FileType('r'),
-                    help="the file to test regex against")
-  parser.add_argument("-n", type=argparse.FileType('rw'),
-                    help="enable NFA output to specified file")
-  parser.add_argument("-d", type=argparse.FileType('rw'),
-                    help="enable DFA output to specified file")
-  args = parser.parse_args()
-  return args;
-
-
-def learnAlphabet(inputFile):
-  alphabet = set()
-  with inputFile as file:
-    line = inputFile.readlines()
-    for i in line:
-      i = i.replace("\n", "")
-      i = i.replace("\r", "")
-      for c in i:
-        alphabet.add(c)
+def learn_alphabet(input_file):
+    alphabet = set()
+    with input_file as file:
+        line = file.readlines()
+        for i in line:
+            i = i.replace("\n", "")
+            i = i.replace("\r", "")
+            for c in i:
+                alphabet.add(c)
     return alphabet
 
 if __name__ == "__main__":
-  main()
+    main()
+    
